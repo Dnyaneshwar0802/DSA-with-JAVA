84b5888cade76997d2eb5326682fd268f00edfa2
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-03-02 13:15:22 +0530

    LuckyNumber

--- Basic before DSA/src/main/java/com/javatpoint/basic/LuckyNumber.java
+++ Basic before DSA/src/main/java/com/javatpoint/basic/LuckyNumber.java
@@ -1,7 +1,30 @@
 package com.javatpoint.basic;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 //5.Lucky Number in Java
 public class LuckyNumber {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        List<Integer> li=new LinkedList<Integer>();
+        for(int i=1;i<=num;i++){
+         li.add(i);
+        }
+        int i=1;
+        while (li.size()>=i){
 
+            for (int j=1;j<=li.size()-1;j++)
+            {
+                //System.out.println(j);
+                if(j%i==0){
+
+                li.remove(j);}
+            }
+            i++;
+            System.out.println(li);
+        }
     }
 }
